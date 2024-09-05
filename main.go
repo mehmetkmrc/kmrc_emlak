273cb12e040f4ee0fcc2c7250832d4323d16e040
Mehmet <mehmetKmrc1109@gmail.com>
2024-09-05 10:44:13 +0300

    feat:Fiber framework added. Trying to parse html templates

--- main.go
+++ main.go
@@ -1,90 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
 	"log"
-	"net/http"
-	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
 )
 
-
-type Page struct{
-	Title string
-	Body []byte
-}
-
 func main(){
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
-
-	http.HandleFunc("/view", viewHandler)
-	http.HandleFunc("/edit", editHandler)
-	http.HandleFunc("/save", saveHandler)
-	log.Fatal(http.ListenAndServe(":8081", nil))
-
-}
+    
+    engine := html.New("./client/templates", ".html")
 
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/view/"):]
-    p, err := loadPage(title)
-	if err != nil {
-        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-        return
-    }
-    renderTemplate(w, "templates/home", p)
-}
+    app := fiber.New(fiber.Config{
+        Views: engine,
+    })
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.Render("home", fiber.Map{
+            "Title": "Go Fiber Template Example",
+        });
+    })
 
-func editHandler(w http.ResponseWriter, r *http.Request){
-	title := r.URL.Path[len("/edit/"):]
-    p, err := loadPage(title)
-    if err != nil {
-        p = &Page{Title: title}
-    }
-    renderTemplate(w, "edit", p)
-}
-
-func saveHandler(w http.ResponseWriter, r *http.Request){
-	title := r.URL.Path[len("/save/"):]
-    body := r.FormValue("body")
-    p := &Page{Title: title, Body: []byte(body)}
-    err := p.save()
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    http.Redirect(w, r, "/view/"+title, http.StatusFound)
-}
-
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-    t, err := template.ParseFiles(tmpl + ".html")
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    err = t.Execute(w, p)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
-}
-
-
-
-
-func (p *Page) save() error{
-	filename := p.Title + ".txt"
-	return os.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-    filename := title + ".txt"
-    body, err := os.ReadFile(filename)
-    if err != nil {
-        return nil, err
-    }
-    return &Page{Title: title, Body: body}, nil
+    log.Fatal(app.Listen(":3000"))
 }
