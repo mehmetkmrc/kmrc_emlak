60197a4f140e959c58d885a2ce31123813dc89e7
Mehmet <mehmetKmrc1109@gmail.com>
2024-10-28 15:06:25 +0300

    feat:refactor

--- internal/infrastructure/adapters/web/static_files.go
+++ internal/infrastructure/adapters/web/static_files.go
@@ -8,4 +8,5 @@ func SetupStaticFiles(app *fiber.App) {
     app.Static("/client/public/fonts", "../../client/public/fonts")
     app.Static("/client/public/images", "../../client/public/images")
     app.Static("client/public/videos", "../../client/public/videos")
+    
 }
